81ecafe69ae48bb0b5a06c574a7e64a2f32c3ce9
nahhhu <nahucordero06@gmail.com>
2025-04-25 18:28:55 +0000

    Creacion del ejercicio de la clase 2

--- TalentoLab/ProyectoEcommerce/src/main/java/inicio/EjercicioClase1.java
+++ TalentoLab/ProyectoEcommerce/src/main/java/inicio/EjercicioClase1.java
@@ -14,5 +14,23 @@ public class EjercicioClase1 {
         int montoTotal = precioProducto  * cantidadProductos;
         System.out.println("Bienvenido" + nombreCliente + ", el monto total a pagar es de: $" + montoTotal);
 
+
+        if (cantidadProductos  > 100){
+            double descuento = montoTotal * 0.15;
+            double montoFinal = montoTotal - descuento;
+            System.out.println("Como su compra supera las 100 unidades ha recibido un descuento del %15. El monto a pagar es de: $" + montoFinal);
+        }
+
+        System.out.println("Hola "+ nombreCliente + ", ingresa un numero por favor");
+        int num = sc.nextInt();
+        for (int i = 0; i   <= num; i++){
+            System.out.println("Los numeros del 1 a tu numero (recorridos con for) son los siguientes: " + i);
+        }
+
+        int cont = 1;
+        while (cont < num){
+            cont++;
+            System.out.println("Los numeros del 1 a tu numero (recorridos con while) son los siguientes: " + cont);
+        }
     }
 }
